306ce754fba1b5750737459ec7ffcf7f33fd6f40
e1b22066 <e1b22066@oit.ac.jp>
2024-10-01 20:30:41 +0900

    じゃんけんの手を選んでCPUと対戦する処理の実装

--- src/main/java/oit/controller/JankenController.java
+++ src/main/java/oit/controller/JankenController.java
@@ -2,6 +2,7 @@ package oit.is.z2722.kaizi.janken.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 //import org.springframework.ui.ModelMap;
 //import org.springframework.web.bind.annotation.GetMapping;
 //import org.springframework.web.bind.annotation.PathVariable;
@@ -14,5 +15,24 @@ public class JankenController {
   public String janken(@RequestParam String username, ModelMap model) {
     model.addAttribute("username", username);
     return "janken.html";
+  }
+
+  @GetMapping("/jankengame")
+  public String jankengame(@RequestParam String hand, ModelMap model) {
+    String result = "結果";
+    if (hand.equals("Gu")) {
+      result = result + " Draw";
+    }
+    if (hand.equals("tyoki")) {
+      result = result + " You Lose";
+    }
+    if (hand.equals("Pa")) {
+      result = result + " You Win!";
+    }
+    model.addAttribute("result", result);
+    model.addAttribute("Player_hand", "あなたの手 " + hand);
+    model.addAttribute("Com_hand", "相手の手 " + "Gu");
+    return "janken.html";
+
   }
 }
