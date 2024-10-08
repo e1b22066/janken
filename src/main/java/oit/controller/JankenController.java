4fe038c1cdc508756136092c5ffd05a16a306c88
e1b22066 <e1b22066@oit.ac.jp>
2024-10-08 18:43:51 +0900

    セキュリティの認証処理の実装

--- src/main/java/oit/controller/JankenController.java
+++ src/main/java/oit/controller/JankenController.java
@@ -1,20 +1,23 @@
 package oit.is.z2722.kaizi.janken.controller;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.ui.ModelMap;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class JankenController {
-  @PostMapping("/janken")
-  public String janken(@RequestParam String username, ModelMap model) {
-    model.addAttribute("username", username);
-    return "janken.html";
+  @GetMapping("/janken")
+  public String janken(@RequestParam(required = false) String username, ModelMap model) {
+    if (username != null) {
+      model.addAttribute("username", username);
+    }
+    return "janken"; // "janken.html" は自動で補完されます
   }
 
   @GetMapping("/jankengame")
